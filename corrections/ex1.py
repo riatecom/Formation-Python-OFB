81f71ed924a0b9146bd6c79681085127c101d39f
Matthieu Viry <matthieu.viry@cnrs.fr>
2023-12-08 17:54:23 +0100

    wip

---
+++ corrections/ex1.py
@@ -0,0 +1,14 @@
+import datetime
+
+# 1. Ouverture du fichier
+with open('data.csv', 'r') as f:
+    data = f.read()
+
+# 2. Corrections
+data = data.replace('coord1', 'latitude')
+data = data.replace('coord2', 'longitude')
+
+# 3. Sauvegarde (dans un fichier différent, en prenant
+#    soin d'ajouter la date du jour dans le nom du fichier)
+with open(f'data-{datetime.date.today()}.csv', 'w') as f:
+    f.write(data)
